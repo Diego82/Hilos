6f3147cd7b5b8b0b3ce14cd24871f94fd215ce9a
diego <djtorrespe@gmail.com>
2015-11-13 07:49:49 +0100

    .

--- Hilos/src/colecciones/TestDiccionario.java
+++ Hilos/src/colecciones/TestDiccionario.java
@@ -3,6 +3,7 @@ package colecciones;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -13,9 +14,11 @@ public class TestDiccionario {
 	public static void main(String[] args) {
 	
 		List<Diccionario> listadoLIST = new ArrayList<Diccionario>();
-		Set<String> listadoSET = new HashSet<String>();
-		String[] array;
-		
+		Set<Diccionario> listadoSET = new HashSet<Diccionario>();
+		HashMap<String, Diccionario> listadoMAP = new HashMap<>();
+		HashMap<String,String> listadoMAP2 = new HashMap<String,String>();
+		HashMap<Integer,String> map = new HashMap<Integer,String>();
+				
 		File inFile = new File("diccionario.csv");
 		String lineaLeida;		
 		Scanner sc;
@@ -24,18 +27,41 @@ public class TestDiccionario {
 			sc = new Scanner(inFile);
 			while (sc.hasNextLine()) {
 				lineaLeida = sc.nextLine();
-				//System.out.println(lineaLeida);
-				array = lineaLeida.split("@");
-				for (int i = 0; i < array.length; i++) {
-					Diccionario aux = new Diccionario(array[0], array[1]);
-					listadoLIST.add(aux);
-				}
+				String[] array = lineaLeida.split("@");
+				Diccionario aux = new Diccionario(array[0], array[1]);
+				listadoLIST.add(aux);
+				listadoSET.add(aux);
+				listadoMAP.put(array[0], aux);
+				listadoMAP2.put(array[0], array[1]);
 			}
-			System.out.println(listadoLIST);
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		int numero = 0;
+		
+		/*System.out.println("tamaño de la lista "+listadoLIST.size());
+		for (int i = 0; i < 15 ; i++) {
+			numero = (int) (Math.random()*listadoLIST.size());
+			System.out.println(listadoLIST.get(numero).getPalabraES()+" en ingles significa "
+								+listadoLIST.get(numero).getPalagraEN()+" y se encuentra en la posicion "+numero);
+		}*/
+		
+		//System.out.println(listadoLIST);
+		//System.out.println(listadoSET);
+		//System.out.println(listadoMAP2);
+		
+		map.put(1, "Casillas");		map.put(15, "Ramos");
+		map.put(1, "Pique");		map.put(5, "Puyol");
+		map.put(11, "Capdevila");	map.put(14, "Xabi Alonso");
+		map.put(16, "Busquets");	map.put(8, "Xavi Hernandez");
+		map.put(18, "Pedrito");		map.put(6, "Iniesta");
+		map.put(7, "Villa");
+		System.out.println(listadoMAP2.size());
+		System.out.println();
+		
 		
 		
 	}
@@ -69,23 +95,7 @@ class Diccionario {
 
 	@Override
 	public String toString() {
-		return "Diccionario [palabraES=" + palabraES + ", palagraEN=" + palagraEN + "]";
-	}
-	
-	
-}
-
-class RecogerPalabras {
-	//Atributo
-	private List<Diccionario> listado1;
-	private Set<Diccionario> listado2;
-	
-	public RecogerPalabras(List<Diccionario> listado1) {
-		this.listado1 = listado1;
-	}
-	public RecogerPalabras(Set<Diccionario> listado2) {
-		this.listado2 = listado2;
-	}
-	
+		return "Diccionario: palabra en español: " + palabraES + ", palagra en ingles: " + palagraEN + "\n";
+	}	
 }
 
